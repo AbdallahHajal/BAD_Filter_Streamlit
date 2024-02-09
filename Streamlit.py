b05f6168a64c4610a66db65b5a253873888ea3d1
Abdallah Abou Hajal <Abdallah.abouhajal@gmail.com>
2024-02-09 15:00:49 +0400

    Update Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -36,6 +36,40 @@ from rdkit import Chem
 from rdkit.Chem import AllChem
 from mordred import Calculator, descriptors
 import pickle
+import requests
+
+def download_file_from_google_drive(id, destination):
+    URL = "https://drive.google.com/uc?export=download"
+
+    session = requests.Session()
+
+    response = session.get(URL, params={'id': id}, stream=True)
+    token = get_confirm_token(response)
+
+    if token:
+        params = {'id': id, 'confirm': token}
+        response = session.get(URL, params=params, stream=True)
+
+    save_response_content(response, destination)    
+
+def get_confirm_token(response):
+    for key, value in response.cookies.items():
+        if key.startswith('download_warning'):
+            return value
+
+    return None
+
+def save_response_content(response, destination):
+    CHUNK_SIZE = 32768
+
+    with open(destination, "wb") as f:
+        for chunk in response.iter_content(CHUNK_SIZE):
+            if chunk: # filter out keep-alive new chunks
+                f.write(chunk)
+
+# Example usage:
+download_file_from_google_drive('1XrReRwiRXEnRNQsFWCv2RIxVGWiDeomj', 'MM_model.pkl')
+download_file_from_google_drive('1cwHTeykD0WQ21gL8mcgtvJeKGMQ5w3M6', 'scaler_MM.pkl')
 
 st.set_page_config(page_title="BAD_Molecule_Filter")
 
